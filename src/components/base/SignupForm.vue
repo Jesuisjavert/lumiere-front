a54ab04cc3beb0de060234c8bfb865a9b50bfe11
강동훈 <jesuisjavert@gmail.com>
2020-06-16 14:34:19 +0900

    signup

---
+++ src/components/base/SignupForm.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <base-info-card
+      :title="title"
+      :subtitle="subtitle"
+      space="4"
+      color="primary"
+    />
+
+    <base-text-field
+      id="username"
+      v-model="signupData.username"
+      label="ID"
+      required
+    />
+
+    <base-text-field
+      id="email"
+      v-model="signupData.email"
+      label="Email"
+    />
+
+    <base-text-field
+      id="password1"
+      v-model="signupData.password1"
+      type="password"
+      label="Password"
+      required
+    />
+
+    <base-text-field
+      id="password2"
+      v-model="signupData.password2"
+      type="password"
+      label="Password confirmation"
+      required
+    />
+
+    <base-btn
+      :color="!theme.isDark ? 'accent' : 'white'"
+      outlined
+      target="_blank"
+      @click="signup"
+    >
+      Signup
+    </base-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseSignupForm',
+    // Injected from the Vuetify Themeable mixin
+    inject: ['theme'],
+
+    props: {
+      subtitle: String,
+      title: {
+        type: String,
+        default: 'SignUp',
+      },
+    },
+    provide () {
+      const my = {}
+      Object.defineProperty(my, 'age', {
+        enumerable: true,
+        get: () => this.age,
+      })
+      return { my }
+    },
+    data () {
+      return {
+        signupData: {
+          username: null,
+          email: null,
+          password1: null,
+          password2: null,
+        },
+      }
+    },
+    methods: {
+      signup () {
+        this.$emit('submit-signup-data', this.signupData)
+        console.log(this.signupData)
+      },
+    },
+  }
+</script>
---
+++ src/views/sections/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-section
+    id="info-alt"
+    space="56"
+  >
+    <v-container>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <base-signup-form title="Sign Up" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </base-section>
+</template>
+
+<script>
+  export default {
+    name: 'SectionSignup',
+  }
+</script>
